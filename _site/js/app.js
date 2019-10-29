f498a6a09f58e280e6b6a13f632fded5a8946dba
kimbongjun <blingface@naver.com>
2019-10-29 17:42:33 +0900

    ㄷㄹㄷㄹㄷㄹㄷㄹㄹㄷㄹㄷ

--- _site/js/app.js
+++ _site/js/app.js
@@ -19,5 +19,33 @@ $(document).ready(function() {
     $('.js-off-canvas-toggle').toggleClass('is-active');
     $('.js-off-canvas-container').toggleClass('is-active');
   });
+  // Add some classes to body for CSS hooks
+
+  var ua = window.navigator.userAgent;
+    var b = "";
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        b = "msie ie" + parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+ 
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        b = "trident ie"+parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+ 
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+        // Edge (IE 12+) => return version number
+        b = "edge ie"+parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+ 
+    }
+ 
+    // other browser
+    if(b!="") {
+        $('body').addClass(b);
+    }
 
 });
